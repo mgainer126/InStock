5f9a5c15195e1dc3ce465d02957d0a74e618699d
Mitchell Cleveland <mitchellclev@outlook.com>
2021-08-06 10:08:04 -0500

    Header styling completes

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,16 +1,15 @@
 // import { Link } from react-router-dom;
 import "./Header.scss";
 import React from "react";
-import logo from "./InStock-Logo.svg";
-
+import logo from "../../assets/images/Logo/InStock-Logo.svg";
 function Header() {
   return (
     <div className="header">
       <div className="header__logo">
-        <img src={logo} alt="logo" />
+        <img className="header__logo--modifier" src={logo} alt="logo" />
       </div>
       <div className="header__links">
-        <p className="header__links--active">WareHouses</p>
+        <p className="header__links--active">Warehouses</p>
         <p className="header__links--inactive">Inventory</p>
       </div>
     </div>
