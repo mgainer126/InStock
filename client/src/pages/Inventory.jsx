483927df190ddb518f225938c3fdd94f42419d9d
Matt Gainer <matt.gainer126@hotmail.com>
2021-08-07 13:35:10 -0400

    tablet list is now a horizontal

--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -12,29 +12,24 @@ function Inventory({ inventoryarr }) {
             <section className="item__sub-arrangement">
               <div>
                 <h4>INVENTORY ITEM</h4>
-                {/* {item.itemName + " " + " > "} */}
                 <li className="item__item">{item.itemName + " " + " > "}</li>
               </div>
               <div>
                 <h4>CATEGORY</h4>
-                {/* {item.category} */}
                 <li className="item__category">{item.category}</li>
               </div>
             </section>
             <section className="item__sub-arrangement">
               <div>
                 <h4>STATUS</h4>
-                {/* {item.status} */}
                 <li className="item__status">{item.status}</li>
               </div>
               <div>
                 <h4>QUANTITY</h4>
-                {/* {item.quantity} */}
                 <li className="item__qty">{item.quantity}</li>
               </div>
               <div>
                 <h4>WAREHOUSE</h4>
-                {/* {item.warehouseName} */}
                 <li className="item__warehouse">{item.warehouseName}</li>
               </div>
             </section>
