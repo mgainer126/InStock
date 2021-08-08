7900d473c2307fa472315dcd7cc5bee142e48322
Mitchell Cleveland <mitchellclev@outlook.com>
2021-08-07 20:48:32 -0500

    Finished up WarehouseList.jsx and associated styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import InventoryList from "./components/InventoryList/InventoryList";
 import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import WarehouseList from "./components/WarehouseList/WarehouseList";
 
 function App() {
   return (
     <>
       <h1> Welcome to Instock</h1>
+      <WarehouseList />
     </>
   );
 }
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+// import { Link } from react-router-dom;
+// import "./Header.scss";
+import React from "react";
+import logo from "../../assets/Logo/InStock-Logo.svg";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img className="header__logo--modifier" src={logo} alt="logo" />
+      </div>
+      <div className="header__links">
+        <p className="header__links--active">Warehouses</p>
+        <p className="header__links--inactive">Inventory</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
